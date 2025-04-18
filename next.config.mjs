a9b6b4417b29706340a040a7ddad4dbceac60406
syedhammad74 <hammadllk@gmail.com>
2025-04-18 21:56:58 +0500

    edit

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  server: {
+    port: 3005,
+    host: '0.0.0.0',
+  },
   images: {
     domains: ["aceternity.com", "cdn.easyfrontend.com"],
   },
