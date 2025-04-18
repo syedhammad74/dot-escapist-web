58dd8b9d580d5f26def5e990b3d9cd8abd6466d3
syedhammad74 <hammadllk@gmail.com>
2025-04-18 21:23:09 +0500

    build

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'dot-escapist-web',
+    script: 'npm',
+    args: 'start',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    instances: 'max',
+    exec_mode: 'cluster',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G'
+  }]
+} 
